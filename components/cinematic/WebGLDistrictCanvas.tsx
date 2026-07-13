285967ec11cc6b4815619f8a7ffea8297724c25b
thatsmelodic <thatsmelodic@gmail.com>
2026-07-13 11:54:38 -0400

    feat(two-harmonic): route district into Beige Dynasty renderer

--- components/cinematic/WebGLDistrictCanvas.tsx
+++ components/cinematic/WebGLDistrictCanvas.tsx
@@ -1,4 +1,13 @@
 'use client';
 
-export { WebGLDistrictCanvasV3 as WebGLDistrictCanvas } from './WebGLDistrictCanvasV3';
-export type { WebGLDistrictCanvasProps } from './WebGLDistrictCanvasV3';
+import { TwoHarmonicDynastyCanvas } from './TwoHarmonicDynastyCanvas';
+import { WebGLDistrictCanvasV3, type WebGLDistrictCanvasProps } from './WebGLDistrictCanvasV3';
+
+export function WebGLDistrictCanvas(props: WebGLDistrictCanvasProps) {
+  if (props.district === 'two-harmonic') {
+    return <TwoHarmonicDynastyCanvas {...props} />;
+  }
+  return <WebGLDistrictCanvasV3 {...props} />;
+}
+
+export type { WebGLDistrictCanvasProps };
