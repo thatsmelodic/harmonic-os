e5224b74185a43aa0934bef3aed8b71e421eea07
thatsmelodic <thatsmelodic@gmail.com>
2026-07-13 12:24:15 -0400

    feat(two-harmonic): wire landmark selection into WebGL map

--- components/cinematic/WebGLDistrictCanvas.tsx
+++ components/cinematic/WebGLDistrictCanvas.tsx
@@ -3,11 +3,17 @@
 import { TwoHarmonicBeigeDynastyCanvas } from './TwoHarmonicBeigeDynastyCanvas';
 import { WebGLDistrictCanvasV3, type WebGLDistrictCanvasProps } from './WebGLDistrictCanvasV3';
 
-export function WebGLDistrictCanvas(props: WebGLDistrictCanvasProps) {
+export type InteractiveWebGLDistrictCanvasProps = WebGLDistrictCanvasProps & {
+  onSelectLandmark?: (index: number) => void;
+};
+
+export function WebGLDistrictCanvas(props: InteractiveWebGLDistrictCanvasProps) {
   if (props.district === 'two-harmonic') {
     return <TwoHarmonicBeigeDynastyCanvas {...props} />;
   }
-  return <WebGLDistrictCanvasV3 {...props} />;
+
+  const { onSelectLandmark: _onSelectLandmark, ...baseProps } = props;
+  return <WebGLDistrictCanvasV3 {...baseProps} />;
 }
 
 export type { WebGLDistrictCanvasProps };
